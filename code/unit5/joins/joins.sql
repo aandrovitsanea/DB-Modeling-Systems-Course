f50ca87bf80c7c30cb057d31f4fb1f5bb1cb6ed0
Anna Androvitsanea <anna.androvitsanea@gmail.com>
2023-11-19 19:55:43 +0100

    Update section of self join to include inner and left join options

--- code/unit5/joins/joins.sql
+++ code/unit5/joins/joins.sql
@@ -24,7 +24,15 @@ FROM employees
 CROSS JOIN departments;
 
 --SELF JOIN
+--can actually be of any type: INNER JOIN, LEFT JOIN, RIGHT JOIN, etc.
+--depends on what you want to achieve with your query.
+
+--INNER JOIN (only employees with managers will be listed)
+SELECT A.name AS EmployeeName, B.name AS ManagerName
+FROM employees A
+JOIN employees B ON A.manager_id = B.employee_id;
+
+--LEFT JOIN (all employees will be listed, with NULL for those without managers)
 SELECT A.name AS EmployeeName, B.name AS ManagerName
 FROM employees A
 LEFT JOIN employees B ON A.manager_id = B.employee_id;
-
