9cf2dcafa0838ca5cef2b38851fceb62ccc4d736
Anna Androvitsanea <anna.androvitsanea@gmail.com>
2023-11-20 13:43:25 +0100

    Add data for FINANCIAL_ADVISOR

--- code/unit5/example/populate_tables.sql
+++ code/unit5/example/populate_tables.sql
@@ -12,7 +12,7 @@ INSERT INTO CUSTOMER (CustomerID, CustomerFirstName, CustomerLastName, CustomerE
 (6, 'Patra', 'Simpson', 'patra.simpson@mygmail.de', '+49 1596 3574', 3),
 (7, 'Elias', 'Simpson', 'elias.simpson@myemail.de', '+49 8866 2247', 4);
 
-INSERT INTO CUSTOMER (CustomerID, CustomerFirstName, CustomerLastName, CustomerEmailAddress, CustomerPhoneNumber, FINANCIAL_ADVISOR_FinancialAdvisorID) VALUES
+INSERT INTO FINANCIAL_ADVISOR (FinancialAdvisorID, FinancialAdvisorFirstName, FinancialAdvisorLastName, FinancialAdvisorEmailAddress, FinancialAdvisorPhoneNumber, BRANCH_BranchID) VALUES
 (1, 'Elias', 'Wagner', 'elias.wagner@myemail.de', '+49 1596 3574', 1),
 (2, 'Elias', 'Meyer', 'elias.meyer@myemail.de', '+49 8525 6545', 1),
 (3, 'Christina', 'Wagner', 'christina.wagner@myemail.de', '+49 9988 7532', 2),
